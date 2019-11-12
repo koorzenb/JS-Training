c1736a31b73df1da2185d895546960054a56dbff
Barend Koorzen <koorzenb@gmail.com>
2019-11-12 17:16:39 +0200

    Created framework for calc.js webcomponent

---
+++ Exercise1/calculator.js
@@ -0,0 +1,13 @@
+/*
+- Create a function that adds two numbers and returns the result. This function should be in a file called calculator.js and exported using es6 modules. 
+- In the index.html file, import the add function and execute it with any two numbers fed to the functions parameters. 
+- Print the result using the "console.log" function. When you open your browser's developer tools you should see your result in the console.
+*/
+
+export class Calculator {
+
+    
+
+}
+
+customElements.define("calculator", Calculator);
