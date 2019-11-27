e4ac25d5320b0c59ab108923e57b37823c2f85b6
Barend Koorzen <koorzenb@gmail.com>
2019-11-27 05:32:02 +0200

    Moved Joe Dirt to 'people.js'

--- Exercise3/people.js
+++ Exercise3/people.js
@@ -5,6 +5,8 @@ Create two new classes that inherit the "person" class.
 The one class should be called JoeDirt and the other MileyCirus. 
 In the constructors of these classes set their name, last name and age properties to appropriate values. 
 Remember to use the super keyword where appropriate. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/super
+and https://javascript.info/class-inheritance
+
 
 Hint: set the "is walking" field on the parent class but the first and last name on the persons. 
     Ensure you call the super on the persons' constructors.
@@ -13,4 +15,15 @@ In the index.html file import JoeDirt and MileyCirus and create instances for ea
     Tell them to move using the start walking function and print out their moving status. 
     Tell them to stop using the stop walking function and print out their moving status. 
 */
+import {Person} from './person.js';
+
+export class JoeDirt extends Person {
+    /*
+    constructor(){
+        this.name = super.name;
+        this.lastName = super.name;
+        this.age = super.age;
+    }   
+    */
+}
 
