8099a193ac8e92b4d9b130c8ecb61773c039ab5e
Barend Koorzen <koorzenb@gmail.com>
2019-11-27 05:28:23 +0200

    testing inheritance

--- Exercise3/person.js
+++ Exercise3/person.js
@@ -1,23 +1,7 @@
-/**       
-Create another file called people.js and import the person class in this file. 
-
-Create two new classes that inherit the "person" class. 
-The one class should be called JoeDirt and the other MileyCirus. 
-In the constructors of these classes set their name, last name and age properties to appropriate values. 
-Remember to use the super keyword where appropriate. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/super
-
-Hint: set the "is walking" field on the parent class but the first and last name on the persons. 
-    Ensure you call the super on the persons' constructors.
-
-In the index.html file import JoeDirt and MileyCirus and create instances for each one. 
-    Tell them to move using the start walking function and print out their moving status. 
-    Tell them to stop using the stop walking function and print out their moving status. 
-*/
-
 export class Person {           
        
     get isWalking() {
-        return this._isWalking == true ? `${this.name} ${this.lastName} is walking` : `${this.name} ${this.lastName} is idle`;
+        return this._isWalking == true ? `\n${this.name} ${this.lastName} is walking` : `\n${this.name} ${this.lastName} is idle`;
     }
 
     constructor(name, lastName, age){
@@ -35,7 +19,55 @@ export class Person {
         this._isWalking = false;          
     }
 }   
+
+/**       
+Create another file called people.js and import the person class in this file. 
+
+Create two new classes that inherit the "person" class. 
+The one class should be called JoeDirt and the other MileyCirus. 
+In the constructors of these classes set their name, last name and age properties to appropriate values. 
+Remember to use the super keyword where appropriate. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/super
+and https://javascript.info/class-inheritance
+
+
+Hint: set the "is walking" field on the parent class but the first and last name on the persons. 
+    Ensure you call the super on the persons' constructors.
+
+In the index.html file import JoeDirt and MileyCirus and create instances for each one. 
+    Tell them to move using the start walking function and print out their moving status. 
+    Tell them to stop using the stop walking function and print out their moving status. 
+*/
+
+export class JoeDirt extends Person {
+    /*
+    constructor(){
+        this.name = super.name;
+        this.lastName = super.name;
+        this.age = super.age;
+    }   
+    */
+}
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   /**Done**
 
   Create a file called person.js. This file should export a class called "Person".
