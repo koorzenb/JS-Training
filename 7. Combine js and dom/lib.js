5a7f46fdb1401f9ed5ff99c0d86d9651aae920e2
Barend Koorzen <BarendK@pragmaworld.net>
2019-12-19 09:10:36 +0200

    Capturing textfield value

--- 7. Combine js and dom/lib.js
+++ 7. Combine js and dom/lib.js
@@ -1,7 +1,17 @@
-export function test() {
-        console.log('working');
+// https://github.com/caperaven/training/blob/master/07.Combine%20javascript%20and%20dom%20project.md
+
+// TODO: remove disabled attribute once all fields have been populated
+
+const name = document.querySelector('.name');
+
+const handle = () => {
+        console.log('clicked here');
+        console.log(name.value);
 }
 
-//TODO: remove disabled attribute once all fields have been populated
+name.addEventListener('click',handle);
 
-https://github.com/caperaven/training/blob/master/07.Combine%20javascript%20and%20dom%20project.md
+
+
+
+
