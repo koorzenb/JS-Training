b8bd8d8db3aa5add76de8996dda8301605a4753e
Barend Koorzen <BarendK@pragmaworld.net>
2019-12-20 10:56:48 +0200

    Registered 1x EL on multiple inputs

--- 7. Combine js and dom/lib.js
+++ 7. Combine js and dom/lib.js
@@ -3,12 +3,42 @@
 // TODO: remove disabled attribute once all fields have been populated
 // TODO: remove global vraiables
 
+const name = document.querySelector('.name');
+const lastName = document.querySelector('.lastName');
+const age = document.querySelector('.age');
 const status = document.querySelector('.status');
-const inputs = document.querySelectorAll('.inputs');
+const input = document.querySelectorAll('input');
+
+function checkValue(e) {
+        status.value = e.currentTarget.value;
+}
+
+input.forEach(function(ev) {
+        ev.addEventListener('input', checkValue);
+});
+
+/*
+const handle = (event) => {
+        console.log('clicked here');
+        status.value += event.currentTarget.value;
+        
+}
+
+name.addEventListener('keyup',handle);
+lastName.addEventListener('keyup',handle);
+window.addEventListener('click', handle );
+
 
 const attachListener = (event) => {
-        status.value = event.currentTarget.value;       
+        status.value = event.currentTarget.value;
+        console.log(surname.value);        
 }
 inputs.forEach(function(textfield) {
-        textfield.addEventListener('keyup',attachListener);         
+        //textfield.addEventListener('keyup',attachListener);
+        console.log(textfield.value);          
 });
+
+function validateInputs() {
+        
+}
+*/
