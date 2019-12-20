875a30e2f1a9f90ddadf7f38f7a37911d570883f
Barend Koorzen <BarendK@pragmaworld.net>
2019-12-20 06:06:27 +0200

    Testing querySelectorAll

--- 7. Combine js and dom/lib.js
+++ 7. Combine js and dom/lib.js
@@ -1,17 +1,14 @@
 // https://github.com/caperaven/training/blob/master/07.Combine%20javascript%20and%20dom%20project.md
 
 // TODO: remove disabled attribute once all fields have been populated
+// TODO: remove global vraiables
 
-const name = document.querySelector('.name');
+const status = document.querySelector('.status');
+const inputs = document.querySelectorAll('.inputs');
 
-const handle = () => {
-        console.log('clicked here');
-        console.log(name.value);
+const attachListener = (event) => {
+        status.value = event.currentTarget.value;       
 }
-
-name.addEventListener('click',handle);
-
-
-
-
-
+inputs.forEach(function(textfield) {
+        textfield.addEventListener('keyup',attachListener);         
+});
