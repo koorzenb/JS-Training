5615a5c95f1d423087e8b0c47b38eb92065b5612
Barend Koorzen <BarendK@pragmaworld.net>
2019-12-22 08:03:03 +0200

    initial

--- 7. Combine js and dom/lib.js
+++ 7. Combine js and dom/lib.js
@@ -1,69 +1,44 @@
-/* eslint-disable quotes */
-// Project 7:
 // https://github.com/caperaven/training/blob/master/07.Combine%20javascript%20and%20dom%20project.md
 
 // TODO: remove disabled attribute once all fields have been populated
+// TODO: remove global vraiables
 
-const name = document.querySelector(".name");
-const surname = document.querySelector(".surname");
-const age = document.querySelector(".age");
-                }
-                return this._age;
-        }
+const name = document.querySelector('.name');
+const lastName = document.querySelector('.lastName');
+const age = document.querySelector('.age');
+const status = document.querySelector('.status');
+const input = document.querySelectorAll('input');
 
-        get status() {
-                if (this._status == null) {
-                        this._status = document.querySelector(".status");
-                }
-                return this._status;
-        }
-
-        get input() {
-                if (this._input == null) {
-                        this._input = document.querySelector(".input");
-                }
-                return this._input;
-        }
-
-        dispose() {
-                this.name = null;
-                this.surname = null;
-                this.age = null;
-        }
-
-        checkValue(e) {
-                this.status.value = e.currentTarget.value;
-        }
-
-        addInputListner() {
-                this.input.forEach(function(ev) {
-                        ev.addEventListener("input", checkValue);
-                });
-        }
+function checkValue(e) {
+        status.value = e.currentTarget.value;
 }
 
+input.forEach(function(ev) {
+        ev.addEventListener('input', checkValue);
+});
+
 /*
-        const handle = (event) => {
-                console.log('clicked here');
-                status.value += event.currentTarget.value;
-                
-        }
+const handle = (event) => {
+        console.log('clicked here');
+        status.value += event.currentTarget.value;
+        
+}
 
-        name.addEventListener('keyup',handle);
-        lastName.addEventListener('keyup',handle);
-        window.addEventListener('click', handle );
+name.addEventListener('keyup',handle);
+lastName.addEventListener('keyup',handle);
+window.addEventListener('click', handle );
 
 
-        const attachListener = (event) => {
-                status.value = event.currentTarget.value;
-                console.log(surname.value);        
-        }
-        inputs.forEach(function(textfield) {
-                //textfield.addEventListener('keyup',attachListener);
-                console.log(textfield.value);          
-        });
+const attachListener = (event) => {
+        status.value = event.currentTarget.value;
+        console.log(surname.value);        
+}
+inputs.forEach(function(textfield) {
+        //textfield.addEventListener('keyup',attachListener);
+        console.log(textfield.value);          
+});
 
-        function validateInputs() {
-                
-        }
-        */
+function validateInputs() {
+        
+}
+*/
