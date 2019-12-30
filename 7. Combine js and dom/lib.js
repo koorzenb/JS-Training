67ce8e0bd5cfa1169b7ee9a43c1570eb57531f51
Barend Koorzen <BarendK@pragmaworld.net>
2019-12-30 21:41:18 +0200

    testing class implementation

--- 7. Combine js and dom/lib.js
+++ 7. Combine js and dom/lib.js
@@ -1,13 +1,34 @@
 // https://github.com/caperaven/training/blob/master/07.Combine%20javascript%20and%20dom%20project.md
-// TODO: Test class implementation - leave till later. Need help with best practice: naming technique + 
-// structure of code. Current code not working, but trying to figure out how to group code. I can't 
-// leave everything as functions. I should rather implement Helper interface? 
+// TODO: Test class implementation - leave till later. Need help with best practice: naming technique +
+// structure of code. Current code not working, but trying to figure out how to group code. I can't
+// leave everything as functions. I should rather implement Helper interface?
 // Or have hierarchy of classes that inherit from each other
 
 class Helper {
 
-        constructor(){
+        // Dummy person
+firstname.value = 'Barend';
+person.surname.value = 'Koorzen';
 
+        get firstname() {
+                if (this._firstname == null) {
+                        this._firstname = document.querySelector('.firstname');
+                }
+                return this._firstname;
+        }
+
+        get surname() {
+                if (this._surname == null) {
+                        this._surname = document.querySelector('.surname');
+                }
+                return this._surname;
+        }
+
+        get age() {
+                if (this._age == null) {
+                        this._age = document.querySelector('.age');
+                }
+                return this._age;
         }
 
         get status() {
@@ -52,8 +73,8 @@ class Helper {
                 this.status = null;
                 this.input = null;
                 this.frmCreatePerson = null;
-                this.btnWalk = null; 
-                this.btnStop = null; 
+                this.btnWalk = null;
+                this.btnStop = null;
         }
 
         checkValue(e) {
@@ -88,70 +109,43 @@ class Person {
         stopWalking() {
                 this._isWalking = false;
         }
-
-        get firstname() {
-                if (this._firstname == null) {
-                        this._firstname = document.querySelector('.firstname');
-                }
-                return this._firstname;
-        }
-
-        get surname() {
-                if (this._surname == null) {
-                        this._surname = document.querySelector('.surname');
-                }
-                return this._surname;
-        }
-
-        get age() {
-                if (this._age == null) {
-                        this._age = document.querySelector('.age');
-                }
-                return this._age;
-        }        
 }
 
+const helper = new Helper();
+const person = new Person(helper.firstname.value, helper.surname.value, helper.age.value);
 
-class Program extends Helper, 
- {
-        // Dummy person
-        model.firstname.value = 'Barend';
-        model.surname.value = 'Koorzen';
+function walkHandler() {
+        const clickHandler = () => {
+                console.log('clicked here');
+        };
 
-        function walkHandler() {
-                const clickHandler = () => {
-                        console.log('clicked here');
-                };
-
-                model.btnWalk.addEventListener('click', clickHandler);
-        }
-
-        function stopHandler() {
-                const clickHandler = () => {
-                        console.log('clicked here');
-                };
-
-                model.btnStop.addEventListener('click', clickHandler);
-        }
-
-        function createPersonHandler() {
-                const clickHandler = e => {
-                        e.preventDefault();
-                        const person = new Person(model.firstname.value, model.surname.value, model.age.value);
-                        model.status.value = `${person.name} ${person.surname} created`;
-                        model.frmCreatePerson.setAttribute('disabled', '');
-                        console.log(model.frmCreatePerson);
-                        // TODO: register seperate eventlistener for statements below to seperate concern
-                        model.btnStop.removeAttribute('disabled');
-                        model.btnWalk.removeAttribute('disabled');
-                        walkHandler();
-                        stopHandler();
-                };
-                model.frmCreatePerson.addEventListener('submit', clickHandler);
-        }
-
-        createPersonHandler();
+        helper.btnWalk.addEventListener('click', clickHandler);
 }
+
+function stopHandler() {
+        const clickHandler = () => {
+                console.log('clicked here');
+        };
+
+        helper.btnStop.addEventListener('click', clickHandler);
+}
+
+function createPersonHandler() {
+        const clickHandler = e => {
+                e.preventDefault();
+                helper.status.value = `${person.name} ${person.surname} created`;
+                helper.frmCreatePerson.setAttribute('disabled', '');
+                console.log(helper.frmCreatePerson);
+                // TODO: register seperate eventlistener for statements below to seperate concern
+                helper.btnStop.removeAttribute('disabled');
+                helper.btnWalk.removeAttribute('disabled');
+                walkHandler();
+                stopHandler();
+        };
+        helper.frmCreatePerson.addEventListener('submit', clickHandler);
+}
+
+createPersonHandler();
 /**
  * Compare the func below to func above. This example breaks code. Why?
  * 
