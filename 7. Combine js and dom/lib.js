281ee553ac12b04128ff2bacdb160e5c34726bd5
Barend Koorzen <BarendK@pragmaworld.net>
2019-12-30 06:30:41 +0200

    working

--- 7. Combine js and dom/lib.js
+++ 7. Combine js and dom/lib.js
@@ -1,27 +1,9 @@
 // https://github.com/caperaven/training/blob/master/07.Combine%20javascript%20and%20dom%20project.md
-// TODO: remove global vraiables
-// TODO: on HTML, add <form>. Test "required" functionality
 
 class Helper {
-        get firstname() {
-                if (this._firstname == null) {
-                        this._firstname = document.querySelector('.firstname');
-                }
-                return this._firstname;
-        }
 
-        get surname() {
-                if (this._surname == null) {
-                        this._surname = document.querySelector('.surname');
-                }
-                return this._surname;
-        }
+        constructor(){
 
-        get age() {
-                if (this._age == null) {
-                        this._age = document.querySelector('.age');
-                }
-                return this._age;
         }
 
         get status() {
@@ -66,8 +48,8 @@ class Helper {
                 this.status = null;
                 this.input = null;
                 this.frmCreatePerson = null;
-                this.btnWalk = null; // dispose
-                this.btnStop = null; // dispose
+                this.btnWalk = null; 
+                this.btnStop = null; 
         }
 
         checkValue(e) {
@@ -102,47 +84,70 @@ class Person {
         stopWalking() {
                 this._isWalking = false;
         }
+
+        get firstname() {
+                if (this._firstname == null) {
+                        this._firstname = document.querySelector('.firstname');
+                }
+                return this._firstname;
+        }
+
+        get surname() {
+                if (this._surname == null) {
+                        this._surname = document.querySelector('.surname');
+                }
+                return this._surname;
+        }
+
+        get age() {
+                if (this._age == null) {
+                        this._age = document.querySelector('.age');
+                }
+                return this._age;
+        }        
 }
 
-const helper = new Helper();
 
-// Dummy person
-helper.firstname.value = 'Barend';
-helper.surname.value = 'Koorzen';
+class Program extends Helper, 
+ {
+        // Dummy person
+        model.firstname.value = 'Barend';
+        model.surname.value = 'Koorzen';
 
-function walkHandler() {
-        const clickHandler = () => {
-                console.log('clicked here');
-        };
+        function walkHandler() {
+                const clickHandler = () => {
+                        console.log('clicked here');
+                };
 
-        helper.btnWalk.addEventListener('click', clickHandler);
+                model.btnWalk.addEventListener('click', clickHandler);
+        }
+
+        function stopHandler() {
+                const clickHandler = () => {
+                        console.log('clicked here');
+                };
+
+                model.btnStop.addEventListener('click', clickHandler);
+        }
+
+        function createPersonHandler() {
+                const clickHandler = e => {
+                        e.preventDefault();
+                        const person = new Person(model.firstname.value, model.surname.value, model.age.value);
+                        model.status.value = `${person.name} ${person.surname} created`;
+                        model.frmCreatePerson.setAttribute('disabled', '');
+                        console.log(model.frmCreatePerson);
+                        // TODO: register seperate eventlistener for statements below to seperate concern
+                        model.btnStop.removeAttribute('disabled');
+                        model.btnWalk.removeAttribute('disabled');
+                        walkHandler();
+                        stopHandler();
+                };
+                model.frmCreatePerson.addEventListener('submit', clickHandler);
+        }
+
+        createPersonHandler();
 }
-
-function stopHandler() {
-        const clickHandler = () => {
-                console.log('clicked here');
-        };
-
-        helper.btnStop.addEventListener('click', clickHandler);
-}
-
-function createPersonHandler() {
-        const clickHandler = e => {
-                e.preventDefault();
-                const person = new Person(helper.firstname.value, helper.surname.value, helper.age.value);
-                helper.status.value = `${person.name} ${person.surname} created`;
-                helper.frmCreatePerson.setAttribute('disabled', '');
-                console.log(helper.frmCreatePerson);
-                // TODO: register seperate eventlistener for statements below to seperate concern
-                helper.btnStop.removeAttribute('disabled');
-                helper.btnWalk.removeAttribute('disabled');
-                walkHandler();
-                stopHandler();
-        };
-        helper.frmCreatePerson.addEventListener('submit', clickHandler);
-}
-
-createPersonHandler();
 /**
  * Compare the func below to func above. This example breaks code. Why?
  * 
