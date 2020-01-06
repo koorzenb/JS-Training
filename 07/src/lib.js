52b0869a17cbd91ce5c4766cd9694b1ff49e0412
Barend Koorzen <BarendK@pragmaworld.net>
2020-01-06 05:20:37 +0200

    basic functionality working

--- 07/src/lib.js
+++ 07/src/lib.js
@@ -9,45 +9,37 @@
 // Should Person have instance of Helper? Or rahter create hierarchy of classes that inherit from each other
 
 class DataCollector {
-        // placeholder person
-        constructor() {
-                // would "this" be necessary?  this.document.querySelector('.firstname').value = 'Joe';
-                document.getElementById('firstname').value = 'Joe';
-                document.getElementById('surname').value = 'Soap';
-                document.getElementById('age').value = '23';
-        }
-
         get txtFirstname() {
                 if (this._firstname == null) {
-                        this._firstname = document.getElementById("firstname").value;
+                        this._firstname = document.querySelector('#firstname').value;
                 }
                 return this._firstname;
         }
 
         get txtSurname() {
                 if (this._surname == null) {
-                        this._surname = document.getElementById('surname').value;
+                        this._surname = document.querySelector('#surname').value;
                 }
                 return this._surname;
         }
 
         get txtAge() {
                 if (this._age == null) {
-                        this._age = document.getElementById('age').value;
+                        this._age = document.querySelector('#age').value;
                 }
                 return this._age;
         }
 
         get txtStatus() {
                 if (this._status == null) {
-                        this._status = document.getElementById('status');
+                        this._status = document.querySelector('#status');
                 }
                 return this._status;
         }
 
         get input() {
                 if (this._input == null) {
-                        this._input = document.getElementById('input');
+                        this._input = document.querySelector('#input');
                 }
                 return this._input;
         }
@@ -61,21 +53,21 @@ class DataCollector {
 
         get btnCreatePerson() {
                 if (this._btnCreatePerson == null) {
-                        this._btnCreatePerson = document.getElementById('createPerson');
+                        this._btnCreatePerson = document.querySelector('#createPerson');
                 }
                 return this._btnCreatePerson;
-        }        
+        }
 
         get btnWalk() {
                 if (this._btnWalk == null) {
-                        this._btnWalk = document.getElementById('walk');
+                        this._btnWalk = document.querySelector('#walk');
                 }
                 return this._btnWalk;
         }
 
         get btnStop() {
                 if (this._btnStop == null) {
-                        this._btnStop = document.getElementById('stop');
+                        this._btnStop = document.querySelector('#stop');
                 }
                 return this._btnStop;
         }
@@ -105,6 +97,19 @@ class Person {
                 this._age = age;
                 this._isWalking = false;
         }
+        // TODO: what do I reference to dispose constructor?
+
+        set name(newValue) {
+                this._name = newValue;
+        }
+
+        set surname(newValue) {
+                this._surname = newValue;
+        }
+
+        set age(newValue) {
+                this._age = newValue;
+        }
 
         get name() {
                 return this._name;
@@ -128,56 +133,64 @@ class Person {
 }
 
 const helper = new DataCollector();
-const person = new Person(helper.txtFirstname, helper.txtSurname, helper.txtAge);
-// TODO: dispose elements
-        function walkHandler() {
-                const clickHandler = () => {
-                        person.startWalking();
-                        helper.txtStatus.innerHTML = `${person.isWalking}`;
-                        helper.txtStatus.removeAttribute('isidle');
-                        helper.txtStatus.setAttribute('class', 'iswalking');
-                        helper.btnWalk.setAttribute('disabled', '');
-                        helper.btnStop.removeAttribute('disabled');
-                };
-                
-                helper.btnWalk.addEventListener('click', clickHandler);
-        }
-        
-        function stopHandler() {
-                const clickHandler = () => {
-                        person.stopWalking();
-                        helper.txtStatus.innerHTML = `${person.isWalking}`;
-                        helper.txtStatus.removeAttribute('iswalking');
-                        helper.txtStatus.setAttribute('class', 'isidle');
-                        helper.btnStop.setAttribute('disabled', '');
-                        helper.btnWalk.removeAttribute('disabled');
-                };
-        
-                helper.btnStop.addEventListener('click', clickHandler);
-        }
-        
-        // Handler to submit inputs on form
-        // function frmCreatePersonHandler() {
-        //         const clickHandler = e => {
-        //                 // e.preventDefault();
-        //         };
-        //         helper.submitCreatePerson.addEventListener('submit', clickHandler);
-        // }
-        
-        function btnCreatePersonHandler() {
-                const clickHandler = () => {
-                        // this.click.bind(this);        // this breaks code
-        
-                        helper.txtStatus.innerHTML = `${person.name} ${person.surname} (${person.age}) created`;
-                        helper.btnCreatePerson.setAttribute('disabled', '');
-                        // TODO: Should I register seperate eventlistener for statements below to seperate concern?
-                        helper.btnStop.removeAttribute('disabled');
-                        helper.btnWalk.removeAttribute('disabled');
-                        walkHandler();
-                        stopHandler();
-                };
-                helper.btnCreatePerson.addEventListener('click', clickHandler);
-        }
-        
-        //frmCreatePersonHandler();
-        btnCreatePersonHandler();
+const person = new Person();
+
+/**
+ * walkHandler problem:
+ * If person created at btnCreatePersonHandler, person obj is destroyed when btnCreatePersonHandler completes and walkHandler cannot access person properties
+ * Solution: pass person? But then at what point do we dispose?
+ */
+
+function walkHandler() {
+        const clickHandler = () => {
+                person.startWalking();
+                helper.txtStatus.innerHTML = `${person.isWalking}`;
+                helper.txtStatus.removeAttribute('isidle');
+                helper.txtStatus.setAttribute('class', 'iswalking');
+                helper.btnWalk.setAttribute('disabled', '');
+                helper.btnStop.removeAttribute('disabled');
+        };
+
+        helper.btnWalk.addEventListener('click', clickHandler);
+}
+
+function stopHandler() {
+        const clickHandler = () => {
+                person.stopWalking();
+                helper.txtStatus.innerHTML = `${person.isWalking}`;
+                helper.txtStatus.removeAttribute('iswalking');
+                helper.txtStatus.setAttribute('class', 'isidle');
+                helper.btnStop.setAttribute('disabled', '');
+                helper.btnWalk.removeAttribute('disabled');
+        };
+
+        helper.btnStop.addEventListener('click', clickHandler);
+}
+
+/**
+ * Problem:
+ * I need to create person form values in textfield once button click. Can do that, but then person obj destroyed
+ * once handler closes and btnWalk needs acccess to person properties
+ * ... or is there a way that I can create empty obj outside click handler, but only fill it later with values?
+ */
+function btnCreatePersonHandler() {
+        const clickHandler = e => {
+                e.preventDefault();
+                // this.click.bind(this);        // this breaks code
+                // TODO: how to use Person constructor. If I use inside this block, obj is destroy once block completes
+                person.name = helper.txtFirstname;
+                person.surname = helper.txtSurname;
+                person.age = helper.txtAge;
+                helper.txtStatus.innerHTML = `${person.name} ${person.surname} (${person.age}) created`;
+                helper.btnCreatePerson.setAttribute('disabled', '');
+                // TODO: Should I register seperate eventlistener for statements below to seperate concern?
+                helper.btnStop.removeAttribute('disabled');
+                helper.btnWalk.removeAttribute('disabled');
+                walkHandler();
+                stopHandler();
+        };
+        helper.btnCreatePerson.addEventListener('click', clickHandler);
+}
+
+btnCreatePersonHandler();
+console.log('working');
