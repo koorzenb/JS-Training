998c2f9355632a27763df60edca09272c8ba818c
Barend Koorzen <koorzenb@gmail.com>
2020-02-07 04:58:58 +0200

    Started ShoppingList

--- 09/src/basics.js
+++ 09/src/basics.js
@@ -1,3 +1,15 @@
 // https://github.com/caperaven/training/blob/master/09.Batch%20dom%20updates%20-%20project.md
 
-console.log('working');
+const shoppingForm = document.querySelector('.shopping');
+const list = document.querySelector('.list');
+
+// We need an array to hold our state
+const items = [];
+
+function handleSubmit(e) {
+    e.preventDefault();
+    console.log('submitted!!');
+    const name = e.currentTarget.item.value;
+    // if its empty, then dont submit it
+    if (!name) return;
+}
