81b8d194173d4ff09c3251452362d7ab3b6c9c1c
Barend Koorzen <koorzenb@gmail.com>
2020-02-21 06:47:26 +0200

    Update viewModel.js

--- 09/src/viewModel.js
+++ 09/src/viewModel.js
@@ -1,15 +1,14 @@
-export class ViewModel{
-
+export class ViewModel {
     get shoppingForm() {
         if (this._shoppingForm == null) {
             this._shoppingForm = document.querySelector('.shoppingForm');
-        }    
+        }
         return this._shoppingForm;
     }
 
     get template() {
         if (this._template == null) {
-        this._template = document.querySelector('#item');
+            this._template = document.querySelector('#item');
         }
         return this._template;
     }
@@ -20,7 +19,7 @@ export class ViewModel{
         }
         return this._list;
     }
-    
+
     set list(newValue) {
         this._list = newValue;
     }
@@ -31,72 +30,46 @@ export class ViewModel{
         }
         return this._entryElements;
     }
-    
+
     set entryElements(newValue) {
         this._entryElements = newValue;
     }
-    
-    dispose(){
-        shoppingForm = null;
-        template = null;
-        list = null;
+
+    dispose() {
+        this.shoppingForm = null;
+        this.template = null;
+        this.list = null;
     }
 
-    constructor(){
-        this.submitHandle = this.submitHandle.
+    constructor() {
+        // registeredEvents = [];
+        // ignore "bind" for now. Ask Rabie later to explain
+        this.submithandler = this._submit.bind(this);
+        this.clickHandler = this._click.bind(this);
     }
 
-    _init(){
+    _init() {
         // properties in options used to track eventlisteners
         const options = {
-            elements: {
-                input: this.shoppingForm,  
-                button: this.entryElements
-            },
+            elements: this.shoppingForm.concat(this.entryElements),
+            // TODO: left on purpose:
+            // {
+            //     input: this.shoppingForm,
+            //     button: this.entryElements
+            // }
+            // is there a way to iterate through obj instead of creating array
+            // and still be able to use in _addEvents?
             eventTypes: {
-                input: "submit",
-                button: "click"
+                input: 'submit',
+                button: 'click',
             },
             callbacks: {
                 input: this.submithandler,
                 button: this.clickHandler,
-                },
-        } 
+            },
+        };
 
         this._addEvents(options);
-    }
-
-    _submitHandle(e) {
-        e.preventDefault();
-        const name = e.currentTarget.item.value;
-        if (!name) return;
-
-        const item = {
-            name,
-            id: Date.now(),
-            complete: false
-        }
-
-        e.
-    }
-
-    _click(event){
-        //event.id = checkbox -> checkboxhandler
-        //    else
-        // close/delete item
-    }
-
-    _displayItems() {
-        
-    }
-
-    _markAsComplete(id) {
-        
-    }
-
-    _deleteItem(id) {
-                    console.log();
-        
     }
 
     /**
@@ -105,17 +78,13 @@ export class ViewModel{
      */
     _addEvents(options) {
         for (const element of options.elements) {
-            // const tag = element.nodeName.toLowerCase();
-            element.addEventListener(
-                options.eventTypes[`${tag}`],
-                options.callbacks[`${tag}`]
-            );
+            const tag = element.nodeName.toLowerCase(); // button/input
+            element.addEventListener(options.eventTypes[`${tag}`], options.callbacks[`${tag}`]);
             this.registeredEvents.push({
-                element: element,
+                element,
                 event: options.eventTypes[`${tag}`],
-                callback: options.callbacks[`${tag}`]
+                callback: options.callbacks[`${tag}`],
             });
+        }
     }
-
-    shoppingForm.addEventListener('submit',submitHandle);
 }
