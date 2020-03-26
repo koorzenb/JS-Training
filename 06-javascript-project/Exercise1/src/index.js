ea44190b41b82714af7243e3cd61a84706c56189
Barend Koorzen <koorzenb@gmail.com>
2020-03-26 04:36:42 +0200

    Added JSDocs

--- 06-javascript-project/Exercise1/src/index.js
+++ 06-javascript-project/Exercise1/src/index.js
@@ -1,3 +1,9 @@
+/**
+ * This function returns the sum of two numbers
+ * @param {int} number1 
+ * @param {int} number2 
+ * @returns {int}
+ */
 export function addNumbers(number1, number2) {
     return number1 + number2;
 }
