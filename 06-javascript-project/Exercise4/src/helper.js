20ec16390da3e99b6a8bf9231ff66904ca536505
Barend Koorzen <koorzenb@gmail.com>
2020-04-03 04:30:10 +0200

    wip

---
+++ 06-javascript-project/Exercise4/src/helper.js
@@ -0,0 +1,3 @@
+export function fetchFiles() {
+    return Promise.all(fetch("./documents/1_p.md"), fetch("./documents/2_p.md"), fetch("./documents/3_p.md"));
+}
--- 06-javascript-project/Exercise4/src/index.js
+++ 06-javascript-project/Exercise4/src/index.js
@@ -1,3 +1,30 @@
-function writeFiles(){
-        console.log();
+import { fetchFiles } from "./helper.js";
+
+export function writeFiles() {
+
+    const fragment = new DocumentFragment();
+    // Spec
+        // 1. write all to template literal
+        // 2. async functions
+
+    // Method
+    // 1. fetch files
+    fetchFiles()
+    .then( files => {
+        
+        for (const file of files) {
+            _appendToFragment(files);            
+        }
+    })
+
+    // 4. insert fragment to template literal
+    // 5. append to HTML
+    
+    
+    // 3. pass to function that adds to fragment
+    function _appendToFragment(fileContent) {
+// 2. extract/convert to text
+        
+        fragment.appendChild(file);
+    }
 }
