c768f04769a62aebb1084f4a7ce4fe0955971abd
Barend Koorzen <BarendK@pragmaworld.net>
2020-04-10 10:22:54 +0200

    Ex4 (ver4) done - using Async/Await

--- 06-javascript-project/Exercise4/src/index.js
+++ 06-javascript-project/Exercise4/src/index.js
@@ -1,19 +1,17 @@
 import { fetchFiles } from "./helper.js";
 
-export function writeFiles() {
+export async function writeFiles() {
     const listOfFiles = ["./documents/1_p.md", "./documents/2_p.md", "./documents/3_p.md"];
     const content = [];
     const p = document.createElement('p')
 
-    fetchFiles(listOfFiles).then(files => {
-        for (const file of files) {
-            file.text()
-            .then( resolve => content.push(resolve) )
-            .then( () => {                    
-                p.innerText = `${content.join('\n')}`;
-                document.querySelector('body').appendChild(p);  
-            })
-        }
-    });
+    const response = await fetchFiles(listOfFiles);
+    
+    for (const file of response) {
+        const resolve = await file.text();
+        content.push(resolve);            
+        p.innerText = `${content.join('\n')}`;
+        document.querySelector('body').appendChild(p);  
+    }
 }
 
