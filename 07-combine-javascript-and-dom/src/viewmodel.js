3333c28da4daf8765be65720118a0078e9868cb8
Barend Koorzen <BarendK@pragmaworld.net>
2020-04-11 15:57:55 +0200

    Update viewmodel.js

--- 07-combine-javascript-and-dom/src/viewmodel.js
+++ 07-combine-javascript-and-dom/src/viewmodel.js
@@ -1,6 +1,15 @@
 export class ViewModel {
     
+    get requiredFields() {
+        if (this._requiredFields == null) {
+            this._requiredFields = document.querySelector(['requiredFields']');
+        }
+        return this._requiredFields;
+    }
     
+    set requiredFields(newValue) {
+        this._requiredFields = newValue;
+    }
     constructor() {
 
     }
@@ -8,9 +17,17 @@ export class ViewModel {
 
 }
 
-// Enabled Create Person button
+
+// Make sure all required fields are populated
+    // validate fields on keyup
+            // EL on required
+            // validation = field != null || undefined 
+// Enable Create Person button
+    // Set "change" (any other event type) event listener on rquired fields
 // Create Person
+    // Get values from required inputs
 // Enabled Start/Stop
+    // submit EL on Create
 // Click Start/Stop walking
 // Update Status
 // Show person walking/idle
