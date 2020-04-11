f8490f82be47dfe84e9a910ca5ea4e5f46b8dc09
Barend Koorzen <BarendK@pragmaworld.net>
2020-04-11 15:41:08 +0200

    Hashing out pseudo

--- 07-combine-javascript-and-dom/src/viewmodel.js
+++ 07-combine-javascript-and-dom/src/viewmodel.js
@@ -1,1 +1,16 @@
-console.log(`working`);
+export class ViewModel {
+    
+    
+    constructor() {
+
+    }
+
+
+}
+
+// Enabled Create Person button
+// Create Person
+// Enabled Start/Stop
+// Click Start/Stop walking
+// Update Status
+// Show person walking/idle
