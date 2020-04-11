5ef105e17c984727dd8774dad806ba6f690eae9d
Barend Koorzen <BarendK@pragmaworld.net>
2020-04-11 15:43:38 +0200

    Added comments

--- 06-javascript-project/Exercise4/src/helper.js
+++ 06-javascript-project/Exercise4/src/helper.js
@@ -7,13 +7,14 @@ export function fetchFiles(files) {
     return Promise.all(fetchedFiles)
 }
 
-function _fileValidation(response) {
-    response => {
-        if(!response.ok){
-            alert(`File not found:\n ${response.url}`);
-        } 
-    }
-}
+// TODO: Create file validation
+// function _fileValidation(response) {
+//     response => {
+//         if(!response.ok){
+//             alert(`File not found:\n ${response.url}`);
+//         } 
+//     }
+// }
 
 // fetch(file) = Promise pending with resolve = response
 // assigning to const b = undefined because resolve is handled by promise
