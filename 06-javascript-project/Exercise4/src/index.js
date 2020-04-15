b9716a99ec466fbef903fb3a7f7768a35028c121
Barend Koorzen <koorzenb@gmail.com>
2020-04-15 04:38:44 +0200

    Implemented review

--- 06-javascript-project/Exercise4/src/index.js
+++ 06-javascript-project/Exercise4/src/index.js
@@ -1,22 +1,30 @@
-export async function renderContent() {
-    const content = [];
-    const p = document.createElement('p')
+export async function main() {
     const response = await fetchFiles(["./documents/1_p.md", "./documents/2_p.md", "./documents/3_p.md"]);
-    
-    for (const file of response) {
-        const resolve = await file.text();
-        content.push(resolve);            
-        p.innerText = `${content.join('\n')}`;
-        document.querySelector('body').appendChild(p);  
-    }
+    const content = await processFiles(response);    
+    renderContent(content);
 }
 
 function fetchFiles(files) {
     const fetchedFiles = [];
-
+    
     for (const file of files) {
         fetchedFiles.push(fetch(file));
     }
     return Promise.all(fetchedFiles)
+}
+
+async function processFiles(response) {
+    const content = [];
+    for (const file of response) {
+        const resolve = await file.text();
+        content.push(resolve);   
+    }    
+    return content;
+}
+
+function renderContent(content) {
+    const p = document.createElement('p')
+    p.innerText = `${content.join('\n')}`;
+    document.querySelector('body').appendChild(p); 
 }
 
