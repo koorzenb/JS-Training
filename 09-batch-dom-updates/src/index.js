aff15863820fa2394f153797779a782a2510cdbf
BarendK <barend.koorzen@pragmaworld.net>
2020-04-19 16:29:18 +0200

    Initial

---
+++ 09-batch-dom-updates/src/index.js
@@ -0,0 +1,10 @@
+// import files you want part of your library here.
+import {MyClass} from "./my-class.js"
+
+
+// export your api here.
+export {MyClass}
+
+console.log(`Devtools:
+try mouse click breakpoints
+try editting in Sources and use Ctrl-S to save and reload page` );
