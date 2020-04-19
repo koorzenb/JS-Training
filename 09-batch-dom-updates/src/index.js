ea9e20048491c90a6a10df75e142d14245e33022
BarendK <barend.koorzen@pragmaworld.net>
2020-04-19 17:04:13 +0200

    Start

--- 09-batch-dom-updates/src/index.js
+++ 09-batch-dom-updates/src/index.js
@@ -8,3 +8,8 @@ export {MyClass}
 console.log(`Devtools:
 try mouse click breakpoints
 try editting in Sources and use Ctrl-S to save and reload page` );
+
+console.log('Also: Audit your page at the end for performance - test memory management and turn off everything except "performance"');
+// https://www.youtube.com/watch?v=5fLW5Q5ODiE&list=PLNYkxOF6rcIC74v_mCLUXbjj7Ng7oTAPE&index=3&t=0s
+
+
---
+++ 09-batch-dom-updates/techspec.md
@@ -0,0 +1,16 @@
+# Proj 9 - Batch updates
+
+## Design layout
+
+<https://cdn.dribbble.com/users/1591610/screenshots/4426221/to-do_shot.jpg>
+
+## Brief
+
+Must:
+
+- _<u>List</u>_ structure - use templates
+- When constructing the list items - use document fragments.
+
+## Full spec at
+
+<https://github.com/caperaven/training/blob/master/09.Batch%20dom%20updates%20-%20project.md>
