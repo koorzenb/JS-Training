3e9f1e97a865d776970509b687f6ddd1098207f0
Barend Koorzen <koorzenb@gmail.com>
2020-05-26 05:23:55 +0200

    Basic list working

--- 09-batch-dom-updates/src/viewmodel.js
+++ 09-batch-dom-updates/src/viewmodel.js
@@ -1,10 +1,27 @@
 export class ViewModel {
+
+get template() {
+    if (this._template == null) {
+        this._template = document.querySelector('template');
+    }
+    return this._template;
+}
+    
     constructor() {
-        console.log(" ViewModel created");
+        this.body = document.querySelector('body');
+        const addHandler = this._add.bind(this);
+        const btnAdd = document.querySelector('button');
+        btnAdd.addEventListener('click', addHandler);         
     }
 
     dispose() {
     }
 
-    
+    _add() {
+        const fragment = new DocumentFragment();
+        const clone = this.template.content.cloneNode(true);
+        const myBody = document.querySelector('body');
+        fragment.appendChild(clone);
+        this.body.appendChild(fragment);
+    }
 }
