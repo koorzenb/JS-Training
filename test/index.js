35e80e788d08efee95d26fb0c7d24a7f85bb065d
Barend Koorzen <koorzenb@gmail.com>
2020-06-04 14:48:10 +0200

    Added testing platform

--- test/index.js
+++ test/index.js
@@ -1,12 +1,1 @@
-function createCellElement(value, className) {
-    const myDiv = document.createElement("div");
-    if (className != null) {
-        myDiv.classList.add(className, "");
-    }
-    myDiv.innerText = value;
-    console.log(myDiv);
-    
-}
-
-createCellElement("barend", done);
-console.log();
+console.log('working');
