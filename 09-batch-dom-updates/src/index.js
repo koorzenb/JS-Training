5acb755daf75d4a3043e0c15b1e6d5e11793359b
Barend Koorzen <koorzenb@gmail.com>
2020-06-04 14:47:59 +0200

    Working on styling

--- 09-batch-dom-updates/src/index.js
+++ 09-batch-dom-updates/src/index.js
@@ -1,21 +1,2 @@
 import {ViewModel} from "./viewmodel.js"
 const viewmodel = new ViewModel();
-
-window.addEventListener("unload",unloadHandler, { once: true });
-
-function unloadHandler() {
-    viewModel.dispose();
-}
-
-/*
-never empty string check!
-add main + li
-move to middle
-adjacent to template
-trim()/
-svg to button  	
-move descrip to utils
-get focus on button
-must have aria next time
-check again around 54min + 1.07
-*/
--- 09-batch-dom-updates/src/viewmodel.js
+++ 09-batch-dom-updates/src/viewmodel.js
@@ -1,3 +1,17 @@
 export class ViewModel {
-    constructor() {}
+    constructor() {
+        /*
+never empty string check!
+add main + li
+move to middle
+adjacent to template
+trim()/
+svg to button  	
+move descrip to utils
+get focus on button
+must have aria next time
+check again around 54min + 1.07
+*/
+
+    }
 }
