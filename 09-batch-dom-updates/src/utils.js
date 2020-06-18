9edf2c72385d690c7e15db43c8863149874da367
Barend Koorzen <koorzenb@gmail.com>
2020-06-18 05:13:36 +0200

    Added OO eventhandling

--- 09-batch-dom-updates/src/utils.js
+++ 09-batch-dom-updates/src/utils.js
@@ -3,3 +3,21 @@ export function formattedDate() {
     const [{value: weekday},,{ value: month },,{ value: day },,{ value: year }] = dateTimeFormat.formatToParts(new Date); 
     return `${weekday} ${month} ${day}, ${year }`;     
 }
+
+const events = [];
+
+export function registerEvent(element, event, callback) {
+    element.addEventListener(event, callback);
+    events.push({
+        element: element,
+        event: event,
+        callback: callback
+    });
+}
+
+export function unregisterEvents() {
+    for (const item of event) {
+        item.element.removeEventListener(item.event, item.callback);
+        item.callback = null;
+    }
+}
--- 09-batch-dom-updates/src/viewmodel.js
+++ 09-batch-dom-updates/src/viewmodel.js
@@ -1,17 +1,48 @@
+import { registerEvent, unregisterEvents } from "./utils.js";
+
 export class ViewModel {
     constructor() {
-        /*
-never empty string check!
-add main + li
-move to middle
-adjacent to template
-trim()/
-svg to button  	
-move descrip to utils
-get focus on button
-must have aria next time
-check again around 54min + 1.07
-*/
+                /*
+        never empty string check!
+        adjacent to template
+        trim()/
+        svg to button  	
+        move descrip to utils
+        get focus on button
+        must have aria next time
+        check again around 54min + 1.07
+        */
+        this.init();
+    }
+
+    init() {
+        this.btnAdd = document.querySelector('button#add');
+        this.btnClose = document.querySelector('button#close');
+        this.addHandler = this._add.bind(this);
+        registerEvent(this.btnAdd,'click', this.addHandler); 
+        this.template = document.querySelector('template');
+    }
+
+    dispose() {
+        this.template = null;
+        this._date = null;
+        unregisterEvents();
+    }
+
+    _add() {
+        const fragment = new DocumentFragment();
+        const clone = this.template.content.cloneNode(true);
+        itemDescription = this.getItemDescription();
+        if(itemDescription.length != 0) {
+            clone.querySelector("#description").innerText = itemDescription;
+            clone.querySelector("#date").innerText = formattedDate();
+            fragment.appendChild(clone);
+            document.body.appendChild(fragment);
+        }
+    }
+
+    getItemDescription() {
 
     }
+
 }
