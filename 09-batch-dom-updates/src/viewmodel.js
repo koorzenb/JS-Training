b622fa735a094d4eaabedf360b5cec51e303a4c0
BarendK <barend.koorzen@pragmaworld.net>
2020-06-19 05:27:51 +0200

    Update viewmodel.js

--- 09-batch-dom-updates/src/viewmodel.js
+++ 09-batch-dom-updates/src/viewmodel.js
@@ -1,11 +1,27 @@
 import { registerEvent, unregisterEvents } from "./utils.js";
 
+get date() {
+    if (this._date == null) {
+        this._date = formattedDate();
+    }
+    return this._date;
+}
+
+get inputs() {
+    if(this._input == null) {
+        this._input = document.querySelector("input#input-details"),
+        this._btnAdd = document.querySelector('button#add');
+        this._btnClose = document.querySelector('button#close');
+    }
+    return {input: this._input, btnadd: this._btnAdd, btnClose: this_btnClose}
+}
+
 export class ViewModel {
     constructor() {
                 /*
         never empty string check!
         adjacent to template
-        trim()/
+        trim() to flatten out ending whitespace
         svg to button  	
         move descrip to utils
         get focus on button
@@ -27,6 +43,9 @@ export class ViewModel {
         this.template = null;
         this._date = null;
         unregisterEvents();
+        this._input = null;
+        this._btnAdd = null;
+        this.btnClose = null;
     }
 
     _add() {
@@ -42,7 +61,7 @@ export class ViewModel {
     }
 
     getItemDescription() {
-
+blah
     }
 
 }
