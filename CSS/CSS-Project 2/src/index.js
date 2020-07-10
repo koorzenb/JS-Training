bb15b69f2b0daa19feb38af362e4e112a22a5546
BarendK <barend.koorzen@pragmaworld.net>
2020-07-10 15:57:31 +0200

    Initial

---
+++ CSS/CSS-Project 2/src/index.js
@@ -0,0 +1,8 @@
+// import files you want part of your library here.
+import {MyClass} from "./my-class.js"
+const myCl = new MyClass();
+
+// export your api here.
+export {MyClass}
+
+console.log('working');
