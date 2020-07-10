b7bd45cc9bac3fc43f6072aaac6010fa6dd28eef
Barend Koorzen <koorzenb@gmail.com>
2020-07-10 18:23:36 +0200

    deleted unnecessary files

--- CSS/CSS-Project 0/src/index.js
+++
@@ -1,8 +0,0 @@
-// import files you want part of your library here.
-import {MyClass} from "./my-class.js"
-const myCl = new MyClass();
-
-// export your api here.
-export {MyClass}
-
-console.log('working');
--- CSS/CSS-Project 0/src/worker.js
+++
@@ -1,8 +0,0 @@
-export function writeMessage(msg) {
-    console.log(msg);
-    const myObj = {
-        last: "koorzen"
-    }
-
-    return myObj;
-}
