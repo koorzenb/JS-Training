d759f91f84e98d940c9262d0ab390f8a87aef219
Barend Koorzen <koorzenb@gmail.com>
2020-10-01 04:04:23 +0200

    Changed data struct

--- accessibility/app/project2/project2.js
+++ accessibility/app/project2/project2.js
@@ -5,89 +5,89 @@ export default class Project2 extends ViewBase {
 // Project: https://github.com/caperaven/training/blob/master/accessibility/03.project2.md
 
     load() {
-        crsbinding.data.updateUI(this._dataId, "items");
+        crsbinding.data.updateUI(this._dataId, "nav");
+        crsbinding.data.updateUI(this._dataId, "main");
+        crsbinding.data.updateUI(this._dataId, "aside");
         crsbinding.data.updateUI(this._dataId, "banner");
         super.load();
     }
 
     async preLoad(setPropertyCallback) {
-        setPropertyCallback("items", this.getRenderData().result);
+        // I had data in flat list, but then had to add "type" property to distinguish. This had side effect of many unused properties in DOM 
+        setPropertyCallback("nav", this.getRenderData().navArray);
+        setPropertyCallback("main", this.getRenderData().mainArray);
+        setPropertyCallback("aside", this.getRenderData().asideArray);
         setPropertyCallback("banner", this.getRenderData().banner);
-        console.log(this.getRenderData().result);
+        console.log(this.getRenderData().navArray);
+        console.log(this.getRenderData().mainArray);
+        console.log(this.getRenderData().asideArray);
     }
 
     getRenderData() {
         // rather have items, nav, main, etc. 
-        const items = [
+        const nav = [
             {
-                type: "navigation",
                 title: "Home",
                 link: "www.google.com"
             },
             {
-                type: "navigation",
                 title: "About",
                 link: "www.google.com"
             },
             {
-                type: "navigation",
                 title: "Contact",
                 link: "www.google.com"
-            },
+            }
+        ]
+
+        const main = [
             {
-                type: "main",
                 title: "Welcome",
                 body: "1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictumst quisque sagittis purus sit amet volutpat consequat. "
             },
             {
-                type: "main",
                 title: "What we do",
                 body: "2Leo a diam sollicitudin tempor id eu. Vitae tortor condimentum lacinia quis. Ipsum a arcu cursus vitae congue. Enim facilisis gravida neque convallis a cras semper."
             },
             {
-                type: "main",
                 title: "Why we do it",
                 body: "3Id diam maecenas ultricies mi eget mauris pharetra et. Mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar."
-            },
-            {
-                type: "aside",
-                title: "Banner",
-            },
-            {
-                type: "aside",
-                title: "Navigation",
-            },
-            {
-                type: "aside",
-                title: "Main",
-            },
-            {
-                type: "aside",
-                title: "Aside",
-            },
-            {
-                type: "aside",
-                title: "Footer",
-            },
-            {
-                type: "footer",
-                title: "Terms",
-                link: "www.google.com"   
-            },
-            {
-                type: "footer",
-                title: "Support",
-                link: "www.google.com"   
             }
         ]
 
-        const result = [];
+        const aside = [
+            {
+                title: "Banner",
+            },
+            {
+                title: "Navigation",
+            },
+            {
+                title: "Main",
+            },
+            {
+                title: "Aside",
+            },
+            {
+                title: "Footer",
+            }
+        ]
+
+        const navArray = [];
+        const mainArray = [];
+        const asideArray = [];
         const banner = ["Logo"];
-        items.forEach(item => {
-            result.push({type: item.type, title: item.title, link: item.link, body: item.body})
+        nav.forEach(item => {
+            navArray.push({title: item.title, link: item.link})
+        })
+        main.forEach(item => {
+            mainArray.push({title: item.title, body: item.body})
+        })
+        aside.forEach(item => {
+            asideArray.push({title: item.title})
         })
 
-        return {banner, result};
+        return {banner, navArray, mainArray, asideArray};
     }
 
 }
