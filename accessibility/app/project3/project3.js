54df9ae9ae4220a49d8676c1927b96c62ef617af
Barend Koorzen <koorzenb@gmail.com>
2020-10-06 05:04:13 +0200

    Started styling

--- accessibility/app/project3/project3.js
+++ accessibility/app/project3/project3.js
@@ -3,6 +3,7 @@ import {ViewBase} from "./../../node_modules/crs-binding/crs-view-base.js";
 export default class Project2 extends ViewBase {
 
 // Project: https://github.com/caperaven/training/blob/master/accessibility/04.project3.md
+// CSS: https://github.com/caperaven/training/blob/master/css/06.project2.md
 
     load() {
         crsbinding.data.updateUI(this._dataId, "nav");
