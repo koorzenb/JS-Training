2cd30298fc17190522a50dc959c8fd7fd37f7746
Barend Koorzen <koorzenb@gmail.com>
2020-10-06 04:41:46 +0200

    Initial

---
+++ accessibility/app/project3/project3.js
@@ -0,0 +1,31 @@
+import {ViewBase} from "./../../node_modules/crs-binding/crs-view-base.js";
+
+export default class Project2 extends ViewBase {
+
+// Project: https://github.com/caperaven/training/blob/master/accessibility/04.project3.md
+
+    load() {
+        crsbinding.data.updateUI(this._dataId, "nav");
+        super.load();
+    }
+
+    async preLoad(setPropertyCallback) {
+        setPropertyCallback("nav", this.getRenderData().navArray);
+        console.log(this.getRenderData().navArray);
+    }
+
+    getRenderData() {
+        const nav = [
+
+        ]
+
+        const navArray = [];
+
+        nav.forEach(item => {
+            navArray.push({title: item.title, link: item.link})
+        })
+
+        return {navArray};
+    }
+
+}
