b5dcf190a3e70eb03bcf56ae472e61cfd53c98b0
Barend Koorzen <BarendK@pragmaworld.net>
2021-01-21 08:11:59 +0200

    Set ev list on li

--- accessibility/Project3/src/viewmodel.js
+++ accessibility/Project3/src/viewmodel.js
@@ -9,16 +9,29 @@ export class ViewModel {
     registerListeners() {
         const lists = document.querySelectorAll("li")
         for (const li of lists) {
-            figure out how to select only parent elements
-            console.log(li);
-            debugger;
             li.addEventListener("mouseover", this.hoverHandler);
         }
     }
 
     hover(e) {
-        debugger;
-        console.log(e.target);
+        console.log(e.currentTarget);
+        const element = e.currentTarget;
+        
+        // function myFunction() {
+            if(element.parentElement != null && element.parentElement.getAttribute("aria-expanded") != "true" ) {
+                element.parentElement.setAttribute("aria-expanded", "true"); 
+            }
+            var x = element.getAttribute("aria-expanded"); 
+            console.log(x);
+            element.setAttribute("aria-expanded", "true"); 
+            // if (x == "true") 
+            // {
+            // x = "false"
+            // } else {
+            // x = "true"
+            // }
+            // document.getElementById("p2").setAttribute("aria-expanded", x);
+            // }
     }
     
     setExpanded(e) {
