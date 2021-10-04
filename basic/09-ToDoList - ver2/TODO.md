2efeb490933e1fe6a9d8542b45ad88cf0f9e016c
Barend Koorzen <koorzenb@gmail.com>
2021-10-04 18:39:15 +0200

    Rita timelogger wip

---
+++ basic/09-ToDoList - ver2/TODO.md
@@ -0,0 +1,7 @@
+get date - if same just overwrite with new values for now
+get start time
+get end time
+
+Change "Today" to "Week x: 31 July - 6 Aug"
+
+Get current time. Suggest to use rounded value to nearest 5min; or manual input 
--- basic/09-ToDoList - ver2/src/viewModel.js
+++ basic/09-ToDoList - ver2/src/viewModel.js
@@ -2,6 +2,17 @@ import { formattedDate, registerEvent, unregisterEvents } from "./utils/system-u
 
 export class ViewModel {
 
+    get entryDate() {
+        const temp = formattedDate();
+        // check for null
+        // check for new date
+        if(this._today == null) {
+            this._today = temp;
+        }
+
+        return this._today !== temp ? temp : this._today;
+    }
+
     constructor() {
         this.init();
         console.log("viewModel started");
@@ -13,6 +24,7 @@ export class ViewModel {
         delete this.itemTemplate;
         delete this.formInput;
         delete this.itemsList;
+        delete this._today
     }
     
     /**
@@ -24,7 +36,7 @@ export class ViewModel {
         this.clickHandler = this._click.bind(this);
         this.formInput = document.querySelector("form input");
         registerEvent(addButton, "click", this.clickHandler);
-        this.itemTemplate = document.querySelector("template#item")
+        this.itemTemplate = document.querySelector("template#item");
     }
 
     /**
@@ -42,10 +54,54 @@ export class ViewModel {
     addItem(event) {
         event.preventDefault();
         const clone = this.itemTemplate.content.cloneNode(true);
-        clone.querySelector("#description").innerText = this.formInput.value;
-        clone.querySelector("#date").innerText = formattedDate();
-        const fragment = new DocumentFragment();
-        fragment.appendChild(clone);
-        this.itemsList.appendChild(fragment);
+        const newEntry = this.calculateHours(this.formInput.value)
+        this.saveToLocalStorage(newEntry);
+        if (newEntry.difference != null) {
+            clone.querySelector("#description").innerText = newEntry.difference;
+            clone.querySelector("#date").innerText = formattedDate();
+            const fragment = new DocumentFragment();
+            fragment.appendChild(clone);
+            this.itemsList.appendChild(fragment);
+        }
+    }
+
+    calculateHours(newValue) {
+// add to storage
+        // check if same date
+            // if so append and save
+            // else start new and save
+
+        // TODO: check if same date exists on local storage -> alert... and overwrite
+
+        
+        //get old
+        // get new
+        //subtract 
+        let currentStorage = localStorage.getItem(this.entryDate)
+        if(currentStorage != null) {
+            currentStorage = JSON.parse(currentStorage);
+        }
+
+
+        const loggedTimes = {};
+        if(currentStorage == null || currentStorage.end != null) {
+            if (currentStorage?.end != null) localStorage.removeItem(this.entryDate);
+            loggedTimes.start = parseInt(newValue);
+            console.log("Starting time saved");
+        } else if (currentStorage.start != null && currentStorage.end == null){
+            loggedTimes.start = currentStorage.start;
+            loggedTimes.end = parseInt(newValue);
+            loggedTimes.difference = loggedTimes.end - loggedTimes.start;
+            console.log("End time saved");
+        }
+        return loggedTimes;
+    }
+
+    saveToLocalStorage(newHours) {
+        const currentStorage = localStorage.getItem(this.entryDate)
+        if (currentStorage != null)
+        alert(`Current value = ${currentStorage}`);
+
+        localStorage.setItem(this.entryDate, JSON.stringify(newHours));
     }
 }
