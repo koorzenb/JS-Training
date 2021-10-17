9857ba11c5fb305cffa8d9bca1c3c7d05c29e987
Barend Koorzen <koorzenb@gmail.com>
2021-10-17 18:14:44 +0200

    Moved files

---
+++ basic/TimeLogger/tests/calculatehours.test.js
@@ -0,0 +1,29 @@
+const ViewModel = require("../src/viewModel");
+
+// describe("Tests for timelogger", () => {
+
+    test("calc difference", async () => {
+        console.log("test");
+        
+    }) 
+// })
+
+// const { addUser, removeUser, getUser, getUsersInRoom } = require('../src/utils/users')
+
+
+// describe("user tests", () => {
+//     let userOne;
+
+//     beforeEach(() => {
+//         userOne = addUser({
+//             id: 22,
+//             username: '  Barend',
+//             room: 'city   '
+//         })
+//     })
+
+//     test("Verify user created", async () => {
+
+//         expect(userOne.user.username).toEqual("barend")
+//         expect(userOne.user.room).toEqual("city")
+//     })
