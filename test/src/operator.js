10c193ac3b60bcd024ca8bc81f01c5e463352396
BarendK <barend.koorzen@pragmaworld.net>
2021-10-20 21:41:48 +0200

    Added Babel

--- test/src/operator.js
+++ test/src/operator.js
@@ -1,7 +1,15 @@
+import { Time } from "./time";
+
 class Operator{
 
     minus(a, b) {
         return a-b;
+    }
+
+    showTime() {
+        const time = new Time();
+        console.log(time.toMinutes(2));
+        console.log("sdf");
     }
 }
 
---
+++ test/src/time.js
@@ -0,0 +1,3 @@
+export function toMinutes(a) {
+    return a * 60;
+}
