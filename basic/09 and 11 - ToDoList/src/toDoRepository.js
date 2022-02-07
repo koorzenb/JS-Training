7e332d11163bedd6f19fbcadf4cc23ec836db104
Barend Koorzen <koorzenb@gmail.com>
2022-02-07 07:11:12 +0200

    Setting up directories

---
+++ basic/09 and 11 - ToDoList/src/toDoRepository.js
@@ -0,0 +1,36 @@
+import {create} from "browser-sync";
+
+class TodoRepository {
+    /**
+     * Action after "+"
+     */
+    create() {
+        // loadComponent("list-item", "ul")
+        // data.records.set(++id, inputvalue)
+        // emit("changed")
+    }
+
+    /**
+     * Action after "enter"
+     */
+    update() {
+        // - get input 
+        // - on keydown/enter
+        // <!-- - data.records = new Map() -->
+        // - data.records.get()
+        // - data.records.set(checkbox.id, inputValue) 
+        // - emit "changed"
+    }
+
+    readById() {
+        //
+    }
+
+    readAll() {
+        //
+    }
+
+    deleteById(id) {
+
+    }
+}
