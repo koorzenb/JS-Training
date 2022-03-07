eb0b760a7002bd1dffda113a3a90714a5376cc6f
Barend Koorzen <koorzenb@gmail.com>
2022-03-07 20:22:58 +0200

    Update list-item.js

--- basic/09 and 11 - ToDoList/src/webComponents/list-item/list-item.js
+++ basic/09 and 11 - ToDoList/src/webComponents/list-item/list-item.js
@@ -2,8 +2,38 @@
 //     - onChangedAttrib dataset.value
 //         - update UI
 //             - this/descriptions.textContent = dataset.value 
-    
+
 //     addNewItem() {
 //         - update datasource.data
 //         - emit("changed")
 //     }
+
+
+// lite version:
+// - listElememnt = createElement(list-item)
+// - listElement.description = descriptiobn
+// - on ConnectedCallback = get description() and populate
+
+
+class ListItem {
+
+    get description() {
+        return this.description;
+    }
+
+    get date() {
+        return this.date;
+    }
+
+    connectedCallback() {
+        const template = 
+    }
+
+    disconnectedCallback() {
+
+    }
+
+
+}
+
+customElements.define("list-item", ListItem);
