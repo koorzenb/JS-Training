eb29f8558bdcd1c21910a9b8294a48a856af626e
Barend Koorzen <koorzenb@gmail.com>
2022-04-03 14:54:35 +0200

    Added localStorage class

--- basic/09 and 11 - ToDoList/src/viewModel.js
+++ basic/09 and 11 - ToDoList/src/viewModel.js
@@ -1,6 +1,8 @@
+import {MyLocalStorage} from './lib/myLocalStorage.js';
 export class ViewModel {
 
     constructor() {
         console.log("viewModel started");
+        const myStorage = new MyLocalStorage("toDoList", "../data/entries.json");
     }
 }
