318b5ace48e01bf94616e9c12aa24a9c71695d89
Barend Koorzen <koorzenb@gmail.com>
2022-04-26 04:02:35 +0200

    Clean

--- basic/12-providers/src/viewModel.js
+++ basic/12-providers/src/viewModel.js
@@ -9,24 +9,14 @@ export class ViewModel {
     dispose() {
         unregisterEvents(addButton, "click");
         delete this.clickHandler;
-        delete this.itemTemplate;
-        delete this.formInput;
-        delete this.itemsList;
     }
 
     /**
      * Initializes view model
      */
     init() {
-        const addButton = document.querySelector("#addItem");
-        this.itemsList = document.querySelector("#list-container");
         this.clickHandler = this.click.bind(this);
-        this.keydownHandler = this.keydown.bind(this);
-        this.formInput = document.querySelector("form input");
         registerEvent(addButton, "click", this.clickHandler);
-        registerEvent(this.formInput, "keydown", this.keydownHandler);
-        this.itemTemplate = document.querySelector("template#item");
-        this.entries = [];
     }
 
     /**
@@ -37,29 +27,5 @@ export class ViewModel {
         if (event.currentTarget.id == "addItem") {
             this.formInput.classList.remove("hidden");
         }
-    }
-
-    keydown(event) {
-        if (event.key != "Enter") return;
-        event.preventDefault();
-        this.addItem();
-    }
-
-    /**
-     * Adds an item to the DOM
-     * @param {*} event 
-     */
-    addItem() {
-        const clone = this.itemTemplate.content.cloneNode(true);
-        const description = this.formInput.value;
-        clone.querySelector("#description").innerText = description;
-        const date = formattedDate();
-        clone.querySelector("#date").innerText = date;
-        const fragment = new DocumentFragment();
-        fragment.appendChild(clone);
-        this.itemsList.appendChild(fragment);
-        this.entries.push({date, description});
-        this.formInput.value = "";
-        this.formInput.classList.add("hidden");
     }
 }
