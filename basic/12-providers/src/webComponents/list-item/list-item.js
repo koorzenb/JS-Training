3044c3e8f87224cd65a58fa687d85bea75d75f2f
Barend Koorzen <koorzenb@gmail.com>
2022-04-26 03:59:49 +0200

    Cleaned up

--- basic/12-providers/src/webComponents/list-item/list-item.js
+++
@@ -1,9 +0,0 @@
-// - script
-//     - onChangedAttrib dataset.value
-//         - update UI
-//             - this/descriptions.textContent = dataset.value 
-    
-//     addNewItem() {
-//         - update datasource.data
-//         - emit("changed")
-//     }
