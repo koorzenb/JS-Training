80ca9bf54ee8bc0881b5a4dd2a4d6279f1d35041
Barend Koorzen <koorzenb@gmail.com>
2022-05-05 09:01:42 +0200

    Initial

--- basic/12-providers/src/viewModel.js
+++ basic/12-providers/src/viewModel.js
@@ -2,8 +2,9 @@ import {registerEvent, unregisterEvents} from "./utils/system-utils.js";
 export class ViewModel {
 
     constructor() {
-        this.init();
+        this._init();
         console.log("viewModel started");
+
         // # You need a provider that deals with the ".bind" syntax on an attribute 
         // DO NOT SEARCH INITIALLY! ASSUME THAT YOU HAVE NAME AND GET MVP
         // - iterate thru attributes and find the one that has the ".bind" syntax. If it has the ".bind" syntax, then you need 
@@ -13,9 +14,9 @@ export class ViewModel {
          * 1. Give element
          * 2. iterate thru element.attributes (element.atributes.filter(attr => attr.name.includes(".bind"))). Write to array
          * 3. For each element in array, get the context value (ie "name" in case of data-name.bind) of the attribute
-         * 4. 
-         * 
-         * 
+         * 4. build a factory that call provider that will return the value of the context.
+         * 5. replace the value of the attribute with the value of the context. (ie data-name="{{name}}"). 
+         *    - parser: convert the value of the context to a string. Find instances of {{}} and replace with the value of the context.
          */
 
 
@@ -29,6 +30,7 @@ export class ViewModel {
     dispose() {
         unregisterEvents(addButton, "click");
         delete this.clickHandler;
+        delete this.element;
     }
 
 
@@ -48,7 +50,10 @@ export class ViewModel {
         }
     }
 
-
+    _init() {
+        this.div = document.querySelector("div");
+        console.log(this.div.attributes);
+    }
 
     _name() {
         const context = {
