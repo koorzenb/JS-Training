8ec61f5f0df10fa554bd267c87c658dfbf3a86f6
Barend Koorzen <koorzenb@gmail.com>
2022-05-14 21:50:33 +0200

    Update viewModel.js

--- basic/12-providers/src/viewModel.js
+++ basic/12-providers/src/viewModel.js
@@ -3,38 +3,30 @@ import Binding from "./binding.js";
 export default class ViewModel {
 
     constructor() {
+        console.log("viewModel started");
         this._init();
+        this._setBindingExpressions();
+    }
+
+    /**
+     * Set binding expressions
+     * @private
+     */
+    _setBindingExpressions() {
         const context = this.locateExpressions(this.div);
         const binding = new Binding;
         const html = binding.textParser(this.div.innerHTML, this[`_${context}`]());
         this.div.innerHTML = html;
         const attribute = binding.textParser(this.div.dataset[`${context}.bind`], this[`_${context}`]());
         this.div.dataset[`${context}.bind`] = attribute;
-        console.log("viewModel started");
-
-        // # You need a provider that deals with the ".bind" syntax on an attribute 
-        // DO NOT SEARCH INITIALLY! ASSUME THAT YOU HAVE NAME AND GET MVP
-        // - iterate thru attributes and find the one that has the ".bind" syntax. If it has the ".bind" syntax, then you need 
-        //  - to use the provider to get the value of the attribute.
-        // 
-        /**
-         * 1. Give element
-         * 2. iterate thru element.attributes (element.atributes.filter(attr => attr.name.includes(".bind"))). Write to array
-         * 3. For each element in array, get the context value (ie "name" in case of data-name.bind) of the attribute
-         * 4. build a factory that call provider that will return the value of the context.
-         * 5. replace the value of the attribute with the value of the context. (ie data-name="{{name}}"). 
-         *    - parser: convert the value of the context to a string. Find instances of {{}} and replace with the value of the context.
-         */
-
-
-        // # and set's that attribute to the value in the context
-        // find {{firstName}} 
-        // fire "name" as context 
-        // You also need a provider to deal with the inner text of a element and replace the "{{property}}" syntax with the actual value.
-
     }
 
     // not sure where else to store this data.
+    /**
+     * Name data
+     * @returns {Object}
+     * @private
+     */
     _name() {
         return {
             firstName: "John",
