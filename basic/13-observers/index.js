a5f886de7daf3c0e6d845a1ee464917379bb17f5
Barend Koorzen <barend.koorzen@pragmaworld.net>
2022-06-05 21:13:40 +0200

    Basic working observer

--- basic/13-observers/index.js
+++ basic/13-observers/index.js
@@ -1,37 +1,5 @@
-import {ViewModel} from "./src/viewModel";
+import {ViewModel} from "./src/viewModel.js";
 const viewModel = new ViewModel;
-
-
-let Subject = {
-    _state: 0,
-    _observers: [],
-    add: function (observer) {
-        this._observers.push(observer);
-    },
-    getState: function () {
-        return this._state;
-    },
-    setState: function (value) {
-        this._state = value;
-        for (let i = 0; i < this._observers.length; i++) {
-            this._observers[i].signal(this);
-        }
-    }
-};
-
-const Observer = {
-    signal: function (subject) {
-        let currentValue = subject.getState();
-        console.log(currentValue);
-    }
-};
-
-Subject.add(Observer);
-Subject.setState(10);
-
-// Notes:
-// Obs is loaded into the Subject - line 3
-// fires signal on line 13 as side effect
 
 // also check:
 // https://excalidraw.com/#json=2R22gM4kcqBBe5w7-GXkw,BhkDEtZZKzCSoTo84B97EA
--- basic/13-observers/src/viewModel.js
+++ basic/13-observers/src/viewModel.js
@@ -2,16 +2,17 @@ import {Person} from "./person.js";
 
 export class ViewModel {
 
-    constructor(person) {
-        this.person = new Person;
+    constructor() {
         this._init();
     }
 
     _init() {
         this.submitButton = document.querySelector('#submit');
         this.submitHandler = this._submit.bind(this);
-        this.submitButton.addEventListener('submit', this.submitHandler);
+        this.submitButton.addEventListener('click', this.submitHandler);
         this.results = document.querySelector('#results');
+        this.person = new Person;
+        this.proxy = new Proxy(this.person, this._setupValidator());
     }
 
     dispose() {
@@ -19,16 +20,69 @@ export class ViewModel {
         this.submitHandler = null;
         this.results = null;
         this.submitButton = null;
+        this.person = null;
     }
 
     _submit(event) {
         event.preventDefault();
-        const name = this.nameInput.value;
-        const lastName = this.lastNameInput.value;
-        const age = this.ageInput.value;
-        this.person.name = name;
-        this.person.lastName = lastName;
-        this.person.age = age;
-        this.results.innerText = this.person.getInfo();
+        const nameInput = document.querySelector('#name');
+        this.name = nameInput.value;
+        const lastNameInput = document.querySelector('#lastName');
+        this.lastName = lastNameInput.value;
+        const ageInput = document.querySelector('#age');
+        this.age = ageInput.value;
+        const valuesToIterate = ["name", "lastName", "age"];
+        this.person.result = true;
+        for (const value of valuesToIterate) {
+            this.proxy[value] = this[`${value}`];
+        }
+        console.log(this.proxy.result);
+        if (this.proxy.result === true) {
+            this.results.textContent = `Found you John Doe`;
+        } else {
+            this.results.textContent = "";
+        }
+    }
+    _setupValidator() {
+        const validator = {
+            set(obj, prop, value) {
+
+                obj[prop] = value;
+                if (obj.result === true) {
+                    if (prop === 'age') {
+                        value = value.toString();
+                        if (value !== "20") {
+                            obj.result = false;
+                        }
+                    }
+
+                    if (prop === 'name') {
+                        if (typeof value !== 'string') {
+                            throw new TypeError('The name is not an string');
+                        }
+                        if (value !== "John") {
+                            obj.result = false;
+                        }
+                    }
+
+                    if (prop === 'lastName') {
+                        if (typeof value !== 'string') {
+                            throw new TypeError('The lastname is not an string');
+                        }
+                        if (value !== "Doe") {
+                            obj.result = false;
+                        }
+                    }
+                }
+
+                if (prop === "age" && value > 30) {
+                    obj[prop] = 30;
+                }
+
+                return true;    // must always be true
+            }
+        };
+
+        return validator;   // return to _init
     }
 }
