d4a6d247999ab5bd4cfc89d0c6b1fd029c9ebf6e
Barend Koorzen <koorzenb@gmail.com>
2022-06-13 20:44:51 +0200

    basic array testing

--- basic/14-fast_array/index.js
+++ basic/14-fast_array/index.js
@@ -1,5 +1,5 @@
-export const processData = (data) => {
-    if (data == null) data = getData();
+export const processData = (data, amount = 1000000) => {
+    if (data == null) data = getData(amount);
     const processor = generateProcessor(data[0]);
     for (const item of data) {
         processor.process(item);
@@ -8,45 +8,77 @@ export const processData = (data) => {
     return processor.result;
 };
 
-export const getData = () => {
-    return [{value: 10, code: "a"}, {value: 10, code: "a"}, {value: 20, code: "a"}, {value: 30, code: "b"}];
+/**
+ * Generates data to be processed
+ * @param amountOfRandomNumbers {number} - amount of random values to generate
+ * @returns {[{code: string, value: number},{code: string, value: number},{code: string, value: number},{code: string, value: number}]}
+ */
+export const getData = (amountOfRandomNumbers = 100) => {
+    // return [{value: 10, code: "a"}, {value: 10, code: "a"}, {value: 20, code: "a"}, {value: 30, code: "b"}];
+    const random = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    const data = [];
+    let count = 1;
+
+    do {
+        const value = random(1, 26);
+        const code = alphabet[random(0, 25)];
+        data.push({value, code});
+        count++;
+    } while (count < amountOfRandomNumbers);
+
+    return data;
 };
 
-const generateProcessor = (firstElement) => {
+export const generateProcessor = (firstElement) => {
     //for firstElement
     // for each property 
     // create results.property
+    const result = {};
+    const keys = Object.keys(firstElement);
+    for (const key of keys) {
+        result[key] = {};
+
+        if (key === "value") {
+            result[key].type = "number";
+            result[key].min = null;
+            result[key].max = null;
+            result[key].avg = null;
+        }
+
+        if (key === "code") {
+            result[key].type = "string";
+            result[key].aggregate = {};
+            result[key].uniqueValues = [];
+        }
+    }
     // will assign results later to results[key] ~~ results.property
     // thus result.value.min = ...
     // maybe do results.value.type = number (???)
 
     const processor = {
-        result: {
-            min: null,
-            max: null,
-            avg: null,
-            countAggregate: {},
-            uniqueValues: []
-        },
         process: (item) => {
-            if (processor.result.min === null || item.value < processor.result.min) { // add numeric check
-                processor.result.min = item.value;
+            if (processor.result.value.min === null || item.value < processor.result.value.min) { // add numeric check
+                processor.result.value.min = item.value;
             }
 
-            if (processor.result.max === null || item.value > processor.result.max) {
-                processor.result.max = item.value;
-            }
+                if (processor.result.value.max === null || item.value > processor.result.max) {
+                    processor.result.value.max = item.value;
+                }
 
-            processor.result.avg = processor.result.avg === null ? item.value : (processor.result.avg + item.value) / 2;
+                processor.result.value.avg = processor.result.value.avg === null ? item.value : (processor.result.value.avg + item.value) / 2;
 
-            if (!processor.result.countAggregate[item.code]) {
-                processor.result.countAggregate[item.code] = 0;
-            }
-            processor.result.countAggregate[item.code]++;
-            if (processor.result.uniqueValues.indexOf(item.code) === -1) {
-                processor.result.uniqueValues.push(item.code);
-            }
-        }
+                if (processor.result.code.aggregate[item.code] == null) {
+                    processor.result.code.aggregate[item.code] = 1;
+                } else {
+                    processor.result.code.aggregate[item.code];
+                }
+
+                if (processor.result.code.uniqueValues.indexOf(item.code) === -1) {
+                    processor.result.code.uniqueValues.push(item.code);
+                }
+        },
+        result
     };
     return processor;
 };
@@ -106,4 +138,6 @@ const generateProcessor = (firstElement) => {
 //  *
 //  */
 
-module.exports = {processData, getData};
+// module.exports = {processData, getData, generateProcessor};
+
+processData();
--- basic/14-fast_array/test/array.test.js
+++ basic/14-fast_array/test/array.test.js
@@ -1,4 +1,4 @@
-const {processData, getData} = require('../index');
+const {processData, getData, generateProcessor} = require('../index');
 
 describe("fast array processing", () => {
     let testArray;
@@ -6,29 +6,64 @@ describe("fast array processing", () => {
         testArray = [{value: 10, code: "a"}, {value: 10, code: "a"}, {value: 20, code: "a"}, {value: 30, code: "b"}];
     });
 
-    test('getData should exist', () => {
-        expect(getData).toBeDefined();
-    });
-
     test('processData should exist', () => {
         expect(processData).toBeDefined();
     });
 
-    test('calc min', () => {
-        const results = processArray(testArray);
-        expect(results.value.min).toBe(10);
-
+    test('getData should exist', () => {
+        expect(getData).toBeDefined();
     });
 
-    test('calc max', () => {
-        const results = processArray(testArray);
-        expect(results.value.max).toBe(30);
+    test("getData sample", () => {
+        const data = getData();
+        expect(data[0].value).toBeDefined;
+        expect(data[0].code).toBeDefined;
     });
 
-    test('calc avg', () => {
-        const results = processArray(testArray);
-        expect(results.value.avg).toBe(17.5);
+    test('processData should return an object', () => {
+        expect(processData()).toBeInstanceOf(Object);
     });
+
+    test('processData should return an result object with the correct properties', () => {
+        const data = getData();
+        const processor = generateProcessor(data[0]);
+        expect(processor.result.value).toHaveProperty('min');
+        expect(processor.result.value).toHaveProperty('max');
+        expect(processor.result.value).toHaveProperty('avg');
+        expect(processor.result.value).toHaveProperty('type');
+        expect(processor.result.code).toHaveProperty('aggregate');
+        expect(processor.result.code).toHaveProperty('uniqueValues');
+        expect(processor.result.code).toHaveProperty('type');
+    });
+
+    // test('generateProcessor returns correct results ', () => {
+    //     const data = getData();
+    //     const processor = generateProcessor(data[0]);
+    //     expect(processor.result.value.type).toBe('number');
+    //     expect(processor.result.value.min).toBeDefined;
+    //     expect(processor.result.value.max).toBeDefined;
+    //     expect(processor.result.value.avg).toBeDefined;
+    //     expect(processor.result.code.type).toBe("string");
+    //     expect(processor.result.code.aggregate).toBeDefined;
+    //     expect(processor.result.code.uniqueValues).toBeDefined;
+
+    // });
+
+    // test('calc min', () => {
+    //     const results = processArray(testArray);
+    //     expect(results.value.min).toBe(10);
+
+    // });
+
+    // test('calc max', () => {
+    //     const results = processArray(testArray);
+    //     expect(results.value.max).toBe(30);
+    // });
+
+    // test('calc avg', () => {
+    //     const results = processArray(testArray);
+    //     expect(results.value.avg).toBe(17.5);
+    // });
 
     // test('calc min', () => {
     //     const results = processArray(testArray);
